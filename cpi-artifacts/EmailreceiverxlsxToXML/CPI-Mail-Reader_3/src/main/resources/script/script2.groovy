69a02180e53e888ddb140bbd7d916f9cd1de86fd
Sindhu <149229579+Project12333@users.noreply.github.com>
2025-12-04 12:42:34 +0530

    Delete cpi-artifacts/EmailreceiverxlsxToXML directory

--- cpi-artifacts/EmailreceiverxlsxToXML/CPI-Mail-Reader_3/src/main/resources/script/script2.groovy
+++
@@ -1,7 +0,0 @@
-// Clear all existing attachments
-message.getAttachments().clear()
-
-// Now add only XML
-def newAttachmentMap = [:]
-newAttachmentMap.put("customerData.xml", dataHandler)
-message.setAttachments(newAttachmentMap)
